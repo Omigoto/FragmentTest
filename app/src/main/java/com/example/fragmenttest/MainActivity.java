85867387bbac386677fa8c346739fbf588a11ecc
jaepeng <pzy163163163@163.com>
2020-11-15 10:27:15 +0800

    Replace Fragment1

--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragmenttest;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -8,14 +9,23 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Fragment1 f1=new Fragment1();
         fragmentTransaction.replace(R.id.container,f1);
         fragmentTransaction.commit();
+    }
+    public void clickReplace(View view){
+        Fragment2 f2=new Fragment2();
+        FragmentTransaction fm=fragmentManager.beginTransaction();
+        fm.replace(R.id.container,f2);
+        fm.commit();
+
     }
 }
