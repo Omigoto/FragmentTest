0d913c617355947c3ded55c45eae8b0a4463e9d6
jaepeng <pzy163163163@163.com>
2020-11-15 10:21:02 +0800

    user Replace initView

--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fragmenttest;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment1 f1=new Fragment1();
+        fragmentTransaction.replace(R.id.container,f1);
+        fragmentTransaction.commit();
     }
 }
